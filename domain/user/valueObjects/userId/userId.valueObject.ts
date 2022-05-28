902063eb500763f49f18cc3a3c7acc3a2b0fbf0f
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-05-28 11:41:00 -0300

    refactor: userId value object extends

--- domain/user/valueObjects/userId/userId.valueObject.ts
+++ domain/user/valueObjects/userId/userId.valueObject.ts
@@ -1,19 +1,19 @@
-import { Result, UniqueEntityID, ValueObject } from 'types-ddd';
+import { Entity, Result, UniqueEntityID } from 'types-ddd';
 
-interface UserIdValueObjectProps{
-  _id: UniqueEntityID
+interface UserIdValueObjectProps {
+	ID?: UniqueEntityID
 }
 
-export class UserIdValueObject extends ValueObject<UserIdValueObjectProps>{
-	private constructor (id?: UniqueEntityID) {
-		super({ _id: id??new UniqueEntityID()});
+export class UserIdValueObject extends Entity<any>{
+	private constructor (props: UserIdValueObjectProps) {
+		super({ ID: props?.ID ?? new UniqueEntityID() }, UserIdValueObject.name);
 	}
 
-	get id ():UniqueEntityID{
-		return this.props._id;
+	get id () {
+		return this._id;
 	}
 
-	static create (id?: UniqueEntityID): Result<UserIdValueObject> {
-		return Result.ok<UserIdValueObject>(new UserIdValueObject(id));
+	static create (ID?: UniqueEntityID): Result<UserIdValueObject> {
+		return Result.ok<UserIdValueObject>(new UserIdValueObject({ ID }));
 	}
 }
