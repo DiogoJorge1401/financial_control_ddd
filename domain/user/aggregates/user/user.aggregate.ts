4a33ae9c317d5134ffc9c363e50f2131be33d473
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-05-28 13:26:40 -0300

    feat: add user.aggregate

--- domain/user/aggregates/user/user.aggregate.ts
+++ domain/user/aggregates/user/user.aggregate.ts
@@ -1,15 +1,20 @@
-import { AggregateRoot } from 'types-ddd';
-import { EmailValueObject, PasswordValueObject, TermValueObject } from '../../valueObjects';
+import { AggregateRoot, Result, UniqueEntityID } from '@/domain/shared';
+import { EmailValueObject, PasswordValueObject, TermValueObject } from '@/domain/user/valueObjects';
 
 interface UserAggregateProps {
-  email: EmailValueObject,
-  password: PasswordValueObject,
-  totalBalanceAvailable: number,
-  terms: TermValueObject
+	email: EmailValueObject
+	password: PasswordValueObject
+	budgetBoxIds?: string[]
+	totalBalanceAvailable: number
+	terms: TermValueObject
 }
 
 export class UserAggregate extends AggregateRoot<UserAggregateProps>{
-	private constructor (props: UserAggregateProps,id?:UniqueEntityID) {
-		super(props, UserAggregate.name);
+	private constructor (props: UserAggregateProps, id?: UniqueEntityID) {
+		super(props, id);
+	}
+
+	static create (props: UserAggregateProps, id?: UniqueEntityID): Result<UserAggregate> {
+		return Result.ok(new UserAggregate(props, id));
 	}
 }
