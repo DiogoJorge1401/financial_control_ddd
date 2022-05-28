1c6112f028b45789314ad96dfc1097138cb680b4
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-05-28 14:30:29 -0300

    feat: create get methods to access all members

--- domain/user/aggregates/user/user.aggregate.ts
+++ domain/user/aggregates/user/user.aggregate.ts
@@ -6,12 +6,27 @@ interface UserAggregateProps {
 	password: PasswordValueObject
 	budgetBoxIds?: string[]
 	totalBalanceAvailable: number
-	terms: TermValueObject
+	terms: TermValueObject[]
 }
 
 export class UserAggregate extends AggregateRoot<UserAggregateProps>{
 	private constructor (props: UserAggregateProps, id?: UniqueEntityID) {
 		super(props, id);
+	}
+	get email (){
+		return this.props.email;
+	}
+	get password (){
+		return this.props.password;
+	}
+	get budgetBoxIds (){
+		return this.props.budgetBoxIds ?? [];
+	}
+	get totalBalanceAvailable ():Readonly<number>{
+		return this.props.totalBalanceAvailable;
+	}
+	get terms (){
+		return this.props.terms;
 	}
 
 	static create (props: UserAggregateProps, id?: UniqueEntityID): Result<UserAggregate> {
