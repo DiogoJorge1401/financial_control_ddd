87e8f9e4aa7c6aa5330b7ed97f09b87fada0c798
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-05-29 07:45:12 -0300

    test: ensure create a valid budgetId value object

---
+++ domain/budgetBox/valueObjects/budgetId/budgetId.valueObject.spec.ts
@@ -0,0 +1,14 @@
+import { UniqueEntityID } from '@/domain/shared/';
+import { BudgetIdValueObject } from './budgetId.valueObject';
+
+describe('budgetId.valueObject', () => {
+	it('should create a valid budget id', () => {
+		const budgetId = BudgetIdValueObject.create();
+		expect(budgetId.isSuccess).toBe(true);
+	});
+	it('should create a valid budget id with provided value', () => {
+		const budgetId = BudgetIdValueObject.create(new UniqueEntityID('valid_id'));
+		expect(budgetId.isSuccess).toBe(true);
+		expect(budgetId.getResult().id.toValue()).toBe('valid_id');
+	});
+});
