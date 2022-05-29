38e284aef829dc529a0282d03fa6d416bccf7a18
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-05-29 08:16:37 -0300

    feat: export reasonId and reasonDescription value objects

--- domain/budgetBox/valueObjects/index.ts
+++ domain/budgetBox/valueObjects/index.ts
@@ -1,3 +1,5 @@
 export * from './budgetDescription/budgetDescription.valueObject';
+export * from './reasonDescription/reasonDescription.valueObject';
 export * from './budgetPercentage/budgetPercentage.valueObject';
 export * from './budgetId/budgetId.valueObject';
+export * from './reasonId/reasonId.valueObject';
