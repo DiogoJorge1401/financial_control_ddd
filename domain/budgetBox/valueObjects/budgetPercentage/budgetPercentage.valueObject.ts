17e92d9f8e43a2f5df1ec1a19663fc882804b967
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-05-29 07:29:49 -0300

    feat: add budgetPercentage value object

---
+++ domain/budgetBox/valueObjects/budgetPercentage/budgetPercentage.valueObject.ts
@@ -0,0 +1,22 @@
+import { ValueObject,Result} from '@/domain/shared';
+
+interface BudgetPercentageValueObjectProps{
+  value:number
+}
+
+export class BudgetPercentageValueObject extends ValueObject<BudgetPercentageValueObjectProps>{
+	private constructor (props:BudgetPercentageValueObjectProps){
+		super(props);
+	}
+
+	get value (){
+		return this.props.value;
+	}
+
+	static create (budgetPercentage:number):Result<BudgetPercentageValueObject>{
+		const isBudgetPercentageValid = budgetPercentage>=0&&budgetPercentage<=100;
+		if(!isBudgetPercentageValid)
+			return Result.fail('Invalid budget percentage, must be between 0 and 100');
+		return Result.ok(new BudgetPercentageValueObject({value:budgetPercentage}));
+	}
+}
