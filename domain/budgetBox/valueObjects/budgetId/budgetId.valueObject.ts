a6308d4c77ef0625adf5ef0dd7f055aba598b922
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-05-29 07:45:35 -0300

    feat: add budgetId value object

---
+++ domain/budgetBox/valueObjects/budgetId/budgetId.valueObject.ts
@@ -0,0 +1,15 @@
+import { Entity, Result, UniqueEntityID } from '@/domain/shared/';
+
+export class BudgetIdValueObject extends Entity<any>{
+	private constructor (id?: UniqueEntityID) {
+		super(null, id);
+	}
+
+	get id () {
+		return this._id;
+	}
+
+	static create (id?: UniqueEntityID): Result<BudgetIdValueObject> {
+		return Result.ok<BudgetIdValueObject>(new BudgetIdValueObject(id));
+	}
+}
