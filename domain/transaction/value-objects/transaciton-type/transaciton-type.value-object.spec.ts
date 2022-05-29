3a02af9d11c3fea51cae80a138cba74ad33774fe
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-05-29 13:24:27 -0300

    test: ensure create a valid transaction type value object

---
+++ domain/transaction/value-objects/transaciton-type/transaciton-type.value-object.spec.ts
@@ -0,0 +1,14 @@
+import { TransactionTypeValueObject } from './transaciton-type.value-object';
+
+describe('transaction-type.value-object', () => {
+  it('should create a valid transaction type', () => {
+    const transactionType = TransactionTypeValueObject.create('ENTRADA');
+    expect(transactionType.isSuccess).toBe(true)
+    expect(transactionType.getResult().value).toBe('ENTRADA')
+  })
+  it('should fail if the provided value is not valid', () => {
+    const transactionType = TransactionTypeValueObject.create('Blah' as any);
+    expect(transactionType.isFailure).toBe(true)
+    expect(transactionType.error).toBe('Invalid transaction type')
+  })
+})
