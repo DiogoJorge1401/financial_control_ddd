87c2083f0f4e0d3702ca88abb647d0f85f14bb6b
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-05-29 08:18:50 -0300

    feat: add reasonId value object

---
+++ domain/budgetBox/valueObjects/reasonId/reasonId.valueObject.ts
@@ -0,0 +1,15 @@
+import { Entity, Result, UniqueEntityID } from '@/domain/shared/';
+
+export class ReasonIdValueObject extends Entity<any>{
+	private constructor (id?: UniqueEntityID) {
+		super(null, id);
+	}
+
+	get id () {
+		return this._id;
+	}
+
+	static create (id?: UniqueEntityID): Result<ReasonIdValueObject> {
+		return Result.ok<ReasonIdValueObject>(new ReasonIdValueObject(id));
+	}
+}
