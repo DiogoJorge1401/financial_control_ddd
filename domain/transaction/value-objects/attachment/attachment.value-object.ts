8c6226c022be2754dbc85f00283f895e65d483f5
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-05-30 14:21:28 -0300

    feat: add attachment.value-object

---
+++ domain/transaction/value-objects/attachment/attachment.value-object.ts
@@ -0,0 +1,25 @@
+import { ValueObject, Result } from '@/domain/shared';
+import isURL from 'validator/lib/isURL';
+
+interface AttachmentValueObjectProps {
+  value: string
+}
+
+export class AttachmentValueObject extends ValueObject<AttachmentValueObjectProps>{
+  private constructor(props: AttachmentValueObjectProps) {
+    super(props)
+  }
+
+  get value() {
+    return this.props.value
+  }
+
+  static create(path: string): Result<AttachmentValueObject> {
+    const regexValidation = /^(.+)\/([^\/]+)$/
+    const isValidPath = regexValidation.test(path)
+    const isValidurl = isURL(path)
+    if (!isValidPath && !isValidurl)
+      return Result.fail('Invalid path')
+    return Result.ok(new AttachmentValueObject({ value: path }))
+  }
+}
