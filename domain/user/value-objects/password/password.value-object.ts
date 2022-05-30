3c8588223cd5ac25dcbf4d0b25583ffb6de9d618
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-05-30 17:23:31 -0300

    feat: add encrypted password

--- domain/user/value-objects/password/password.value-object.ts
+++ domain/user/value-objects/password/password.value-object.ts
@@ -1,23 +1,38 @@
 import { Result, ValueObject } from '@/domain/shared/core';
+import { compare, hash } from 'bcrypt';
 
 interface PasswordValueObjectProps {
-  value: string
+	value: string
 }
 
 export class PasswordValueObject extends ValueObject<PasswordValueObjectProps>{
-	private constructor (props: PasswordValueObjectProps) {
+	private _isEncrypted: boolean
+
+	private constructor(props: PasswordValueObjectProps) {
 		super(props);
+		this._isEncrypted = false
 	}
 
-	get value (): string {
-		return this.props.value;
+	get value(): string { return this.props.value; }
+
+	get isEncrypted(): boolean { return this._isEncrypted }
+
+	async encryptPassword(): Promise<void> {
+		const salt = 10
+		this.props.value = await hash(this.value, salt);
+		this._isEncrypted = true
 	}
 
-	static create (password: string): Result<PasswordValueObject> {
+	async compare(candidate: string): Promise<boolean> {
+		if (!this.isEncrypted)
+			return candidate === this.value
+		return await compare(candidate, this.value)
+	}
+
+	static create(password: string): Result<PasswordValueObject> {
 		const isValidPasswordLength = password.length >= 3 && password.length <= 20;
 		if (!isValidPasswordLength)
 			return Result.fail('Password must have minimum 3 and max 20 characters');
-
 		return Result.ok<PasswordValueObject>(new PasswordValueObject({ value: password }));
 	}
 }
