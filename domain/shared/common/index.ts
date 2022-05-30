3e910a140ca649e69bd9bc46b6009c247d3c31d1
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-05-30 15:35:03 -0300

    refactor: move userid value object to shared context

--- domain/shared/common/index.ts
+++ domain/shared/common/index.ts
@@ -1,3 +1,4 @@
 export * from './date/date.value-object';
 export * from './reason-id/reason-id.value-object';
 export * from './budget-id/budget-id.value-object';
+export * from './userId/user-id.value-object';
--- domain/user/value-objects/index.ts
+++ domain/user/value-objects/index.ts
@@ -2,4 +2,3 @@ export * from './email/email.value-object';
 export * from './ip/ip.value-object';
 export * from './password/password.value-object';
 export * from './term/term.value-object';
-export * from './userId/user-id.value-object';
