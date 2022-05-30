855f0301fe6ff2e5fc622b6515a1c110dc9db5cb
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-05-30 14:18:46 -0300

    refactor: fix word

--- domain/transaction/value-objects/transaction-note/transaction-note.value-object.spec.ts
+++ domain/transaction/value-objects/transaction-note/transaction-note.value-object.spec.ts
@@ -6,14 +6,14 @@ describe('transaction-note.value-object', () => {
     expect(note.isSuccess).toBe(true)
     expect(note.getResult().value).toBe('valid_note')
   })
-  it('should fail if provid a text longer than 144 characters', () => {
+  it('should fail if provide a text longer than 144 characters', () => {
     const note = TransactionNoteValueObject.create(
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro suscipit ad nisi fuga nobis animi eveniet reprehenderit eaque maiores consequatur.'
     )
     expect(note.isFailure).toBe(true)
     expect(note.error).toBe('Note must have minimum 4 and max 144 characters')
   })
-  it('should fail if provid a text smaller than 4 characters', () => {
+  it('should fail if provide a text smaller than 4 characters', () => {
     const note = TransactionNoteValueObject.create('123')
     expect(note.isFailure).toBe(true)
     expect(note.error).toBe('Note must have minimum 4 and max 144 characters')
