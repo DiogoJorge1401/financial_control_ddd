47136141d7a8d5c87caab5493b2747891e8ec8e0
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-05-31 19:43:19 -0300

    refactor: delete infra directory

--- src/infra/repositories/user/interfaces/user-persistence.interface.ts
+++
@@ -1,9 +0,0 @@
-import { TermPersistence } from './terms-persistence.interface'
-
-export interface IUserPersistence{
-  email: string
-  password: string
-  budgetBoxIds?: Array<string>
-  totalBalanceAvailable: number
-  terms: TermPersistence[]
-}
--- src/infra/repositories/user/interfaces/user-repository.interface.ts
+++
@@ -1,6 +0,0 @@
-import { IGenericRepository } from '@repositories/shared';
-import { IUserPersistence } from './user-persistence.interface';
-
-export interface IUserRepository<ORM> extends IGenericRepository<IUserPersistence> {
-  getConnection(): ORM
-}
