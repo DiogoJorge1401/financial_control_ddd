0e90c628fb48c122273855dbfca63305c29db466
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-06-05 07:02:57 -0300

    chore: set environment variables

---
+++ src/config/env.ts
@@ -0,0 +1,13 @@
+export const JWT_SECRET_KEY = process.env['JWT_SECRET_KEY'] || 'secret';
+
+export const DB_NAME = process.env['DB_NAME'] || 'financial-control';
+
+export const MONGO_PASSORD = process.env['MONGO_PASSORD'] || 'mongo';
+
+export const MONGO_USER = process.env['MONGO_USER'] || 'root';
+
+export const MONGO_HOST = process.env['MONGO_HOST'] || '127.0.0.1';
+
+export const MONGO_PORT = process.env['MONGO_PORT'] || 27017;
+
+export const APP_PORT = process.env['APP_PORT'] || 3000;
