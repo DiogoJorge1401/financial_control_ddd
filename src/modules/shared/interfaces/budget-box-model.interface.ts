2b9423e73188038448bccd9277519fa93c8499bf
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-06-08 17:09:57 -0300

    feat(interface) create budget box interface #17

---
+++ src/modules/shared/interfaces/budget-box-model.interface.ts
@@ -0,0 +1,26 @@
+import { IReason } from './reason-model.interface'
+import { ICurrency } from './transaction-model.interface'
+
+export interface IBudgetBox {
+  readonly id: string
+
+  ownerId: string
+
+  description: string
+
+  balanceAvailable: ICurrency
+
+  isPercentage: boolean
+
+  budgetPercentage: number
+
+  reasons: Array<IReason>
+
+  createdAt: Date
+
+  updatedAt: Date
+
+  isDeleted?: boolean
+
+  deletedAt?: Date
+}
